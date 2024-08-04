c5038ac9d9dbf723d8675d19cbef872083978b77
Cuboidigon . <1steverchessguy@gmail.com>
2024-08-04 22:38:56 +0000

    transferred files from non repo to repo

---
+++ CSS/RES/INC/header.php
@@ -0,0 +1,7 @@
+<header>
+    <div class="navDiv"><a>test</a> <a>test2</a></div>
+    <div style="display: flex;" class="titleDiv">
+        <h1>The Website of Many Features</h1>
+        <textarea class="search" placeholder="search..." style="height: 19px;"></textarea>
+    </div>
+</header>
---
+++ foodReviews.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="CSS/main.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap">
+<title>The Website of Many Features</title>
+<link rel="icon" type="image/x-icon" href="CSS/RES/The Website of Many Features Icon.svg">
+</head>
+<body>
+<?php include 'CSS/RES/INC/header.php';?>
+<?php include 'CSS/RES/INC/footer.php';?>
+</body>
+<html>
